80b43dce4ad3b02fcf135c915390ab3ba0100743
personontheinternet1234 <135486733+personontheinternet1234@users.noreply.github.com>
2023-08-26 13:24:41 -1000

    Added more comments, cleaned up code a bit.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import math_functions
 import nodes
 
+# Variables in camelCase
+# Classes Uppercase Letters
+# functions_underscored
--- nodes.py
+++ nodes.py
@@ -1,30 +1,30 @@
-# Variables in camelCase
-# Classes Uppercase Letters
-# functions_underscored
-
-learningRate = 0.1
-
-
-
 class Graph:
+    # Overall graph. Not sure when I'm going to use this. Useful mainly just to list all the nodes.
+
     def __init__(self, name):
         self.name = name
         self.nodes = []
-        
+
     def add_node(self, name):
         self.nodes.append(name)
 
+
 class Node:
+    # Each Node. Very useful. node.activationEnergy will be changed a lot.
+
     def __init__(self, name, energy=0):
         self.name = name
         self.activationEnergy = energy
         self.connections = []
+
     def new_connection(self, name, destination, weight, bias):
         name = Connection(name, destination, weight, bias)
         self.connections.append(name)
 
-        
+
 class Connection:
+    # Connections for a node. Very useful. Much math will be done with connections.
+
     def __init__(self, origin, destination, weight, bias):
         self.origin = origin
         self.destination = destination
@@ -33,6 +33,7 @@ class Connection:
 
     def return_name(self):
         return str(self.origin.name + " to " + self.destination.name)
+
 
 if __name__ == "__main__":
     # testing info:
