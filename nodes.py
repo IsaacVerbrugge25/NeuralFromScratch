a8bb9de7ecb6a61d0bdfa33514a7abbfb17fbcf7
personontheinternet1234 <135486733+personontheinternet1234@users.noreply.github.com>
2023-08-26 13:17:19 -1000

    Separated nodes.py and math_function.py into their own modules imported into main.py where our specific neural net will really be made

--- nodes.py
+++ nodes.py
@@ -1,41 +1,10 @@
-import math
-import matplotlib
-
 # Variables in camelCase
 # Classes Uppercase Letters
 # functions_underscored
 
 learningRate = 0.1
 
-def RelU(input): #Rectified linear activation function
-    if input > 0:
-        return input
-    else:
-        return 0
 
-def calculate(incoming_energy, weight, bias=0): # simple calculate function to find the activation energy of a new node given a connection and an incoming energy
-    return(incoming_energy * weight + bias)
-
-# def in_out(polynomial): # takes a polynomial (hopefully we can graph the neural network's guesses as y with a given input as x) and calculates y for x
-#         sum = 0
-#         for i in range(len(polynomial.powers)):
-#             sum += polynomial.coefficients[i] * (x_one ** function.powers[i])
-#         return sum
-
-def SSR(observedDataSetCurveFormula, actualDataSet): # sum of squared values function
-    sum = 0
-    for value in actualDataSet:
-        # sum += ( in_out(observedDataSetCurveFormula, value)  - value ) ** 2 # for each x value
-        ...
-
-
-def new_value(oldWeight): # gradient descent function for a given connection's weight.
-    # take derivitive of sum of squared values with respect to w4:
-    # TODO
-    derivitive = ...
-
-    # returns next weight or bias the connection should have
-    return oldWeight - (derivitive * learningRate)
 
 class Graph:
     def __init__(self, name):
@@ -65,22 +34,20 @@ class Connection:
     def return_name(self):
         return str(self.origin.name + " to " + self.destination.name)
 
-## testing info:
-mygraph = Graph("mygraph")
-HI = Node("HI")
-CA = Node("CA")
-
-mygraph.add_node(HI.name)
-mygraph.add_node(CA.name)
-
-HI.new_connection(HI, CA, 0.2, 0)
-
-print(f"Hawaii's connections: ")
-for connection in HI.connections:
-    print(connection.return_name(), sep=", ", end="")
-print("\n")
-
-print(f"Nodes in graph: {mygraph.nodes}")
-
 if __name__ == "__main__":
-    ...
+    # testing info:
+    mygraph = Graph("mygraph")
+    HI = Node("HI")
+    CA = Node("CA")
+
+    mygraph.add_node(HI.name)
+    mygraph.add_node(CA.name)
+
+    HI.new_connection(HI, CA, 0.2, 0)
+
+    print(f"Hawaii's connections: ")
+    for connection in HI.connections:
+        print(connection.return_name(), sep=", ", end="")
+    print("\n")
+
+    print(f"Nodes in graph: {mygraph.nodes}")
